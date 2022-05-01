4982b29b6f5d7ce1ca663d5488fc67ef84defb61
Olaf <olaf.szym123@gmail.com>
2022-05-01 15:07:16 +0200

    Server file clean up.

--- api/server.go
+++ api/server.go
@@ -13,14 +13,7 @@ import (
 	"github.com/olafszymanski/user-cms/graph/generated"
 )
 
-const defaultPort = "8080"
-
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
 	c := generated.Config{Resolvers: &graph.Resolver{}}
 	c.Directives.IsAuth = graph.IsAuth
 
@@ -29,6 +22,5 @@ func main() {
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", handler.NewDefaultServer(generated.NewExecutableSchema(c)))
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, router))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("API_PORT"), router))
 }
